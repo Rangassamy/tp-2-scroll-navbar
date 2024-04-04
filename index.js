23483420de98ecef81ab89bb45a88d4967439d3c
Rangassamy <enzorangassamy@gmail.com>
2024-04-04 10:17:07 -0400

    version finish without correction

--- index.js
+++ index.js
@@ -5,3 +5,17 @@
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
 // Connaitre niveau de scroll (window.scrollY)
+
+//------------------------------------------------------//
+
+window.addEventListener("scroll", () => {
+  const navValue = setTimeout(() => {
+    window.scrollY;
+  }, 1);
+
+  if (navValue > window.scrollY) {
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top = "-60px";
+  }
+});
